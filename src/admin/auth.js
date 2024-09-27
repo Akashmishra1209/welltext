36357579d0574c623a45ba1f417dc8ffa420d635
Akash <130904930+Akashmishra1209@users.noreply.github.com>
2024-09-27 10:24:06 +0000

    Improved Login Page

--- src/admin/auth.js
+++ src/admin/auth.js
@@ -16,7 +16,7 @@ export const login = (email, password) => {
 };
 
 export const isAuthenticated = () => {
-    return localStorage.getItem('isAuthenticated') === 'true';
+    return localStorage.getItem('isAuthenticated') == 'true';
 };
 
 export const logout = () => {
