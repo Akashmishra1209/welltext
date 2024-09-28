028d4b40f326d75ac5946c7a60e7024638c4e712
Akash <130904930+Akashmishra1209@users.noreply.github.com>
2024-09-28 12:06:33 +0000

    Maked The Admin Page Better In UI And Added Hamburger Menu

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,5 +1,5 @@
 // src/admin/Admin.js
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Dashboard from './Dashboard';
@@ -14,10 +14,13 @@ import Calculator from "./Calculator"
 import BookmarkManager from './BookmarkManager';
 import QRCodeGenerator from './QRCodeGenerator';
 import Whiteboard from "./Whiteboard"
+import { useMediaQuery } from '@mui/material';
 
-const Admin = () => {
+export default function Admin(){
+    const isLargeDevice = useMediaQuery('(min-width:1200px)')
+
     useEffect(() => {
-     document.getElementsByTagName("nav")[0].remove()
+        isLargeDevice && document.getElementsByTagName("nav")[0]?.remove()
     }, [])
     if (!isAuthenticated()) {
         return <Navigate to="/login" />;
@@ -26,23 +29,21 @@ const Admin = () => {
     return (
         <div className="d-flex">
             <Sidebar />
-            <div className="flex-grow-1 p-3"style={{marginLeft:"100px"}}>
+            <div className={`flex-grow-1 ${isLargeDevice&& 'p-3'}`} style={{ marginLeft: isLargeDevice ? "100px" : "0" }}>
                 <Routes>
                     <Route path="dashboard" element={<Dashboard />} />
                     <Route path="users" element={<Users />} />
                     <Route path="settings" element={<Settings />} />
-                    <Route path="add" element={<Addblog/>}/>
-                    <Route path="list" element={<ListBlog/>}/>
-                    <Route path="todo" element={<ToDo/>}/>
-                    <Route path="stopwatch" element={<Stopwatch/>}/>
-                    <Route path="calculator" element={<Calculator/>}/>
-                    <Route path="bmanager" element={<BookmarkManager/>}/>
-                    <Route path="qrcode" element={<QRCodeGenerator/>}/>
-                    <Route path="whiteboard" element={<Whiteboard/>}/>
+                    <Route path="add" element={<Addblog />} />
+                    <Route path="list" element={<ListBlog />} />
+                    <Route path="todo" element={<ToDo />} />
+                    <Route path="stopwatch" element={<Stopwatch />} />
+                    <Route path="calculator" element={<Calculator />} />
+                    <Route path="bmanager" element={<BookmarkManager />} />
+                    <Route path="qrcode" element={<QRCodeGenerator />} />
+                    <Route path="whiteboard" element={<Whiteboard />} />
                 </Routes>
             </div>
         </div>
     );
 };
-
-export default Admin;
--- src/admin/Sidebar.js
+++ src/admin/Sidebar.js
@@ -1,20 +1,30 @@
 import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { logout } from './auth';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, IconButton } from '@mui/material';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Typography, IconButton, Button, useMediaQuery } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { AccessAlarm, Bookmark, Calculate, Gesture, QrCode } from '@mui/icons-material';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { Done, Edit, Newspaper } from '@mui/icons-material';
+import { Done, Edit, Newspaper, MenuRounded } from '@mui/icons-material';
 
 export default function Sidebar() {
   const [open, setOpen] = useState(true);
 
+  const isLargeDevice = useMediaQuery('(min-width:1200px)')
+
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleToggle = () => {
+    if (open == true) {
+      setOpen(false)
+    } else {
+      setOpen(true)
+    }
+  }
 
   const handleLogout = () => {
     logout();
@@ -22,79 +32,89 @@ export default function Sidebar() {
   };
 
   return (
-    <Drawer variant="permanent" open={open}>
-      <div className="d-flex flex-column p-3" style={{ height: '100vh' }}>
-        <Typography variant="h6">Admin Panel</Typography>
-        <List>
-          <ListItem button component={NavLink} to="/admin/dashboard">
-            <ListItemIcon>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/list">
-            <ListItemIcon>
-              <Newspaper />
-            </ListItemIcon>
-            <ListItemText primary="All Blogs" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/users">
-            <ListItemIcon>
-              <PeopleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Users" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/settings">
-            <ListItemIcon>
-              <SettingsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Settings" />
-          </ListItem>
-          <Typography variant="h6" className='ml-4'>Apps</Typography>
-          <ListItem button component={NavLink} to="/admin/todo">
-            <ListItemIcon>
-              <Done />
-            </ListItemIcon>
-            <ListItemText primary="To Do" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/stopwatch">
-            <ListItemIcon>
-              <AccessAlarm />
-            </ListItemIcon>
-            <ListItemText primary="Clock" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/calculator">
-            <ListItemIcon>
-              <Calculate />
-            </ListItemIcon>
-            <ListItemText primary="Calculator" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/bmanager">
-            <ListItemIcon>
-              <Bookmark />
-            </ListItemIcon>
-            <ListItemText primary="Bookmark" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/qrcode">
-            <ListItemIcon>
-              <QrCode />
-            </ListItemIcon>
-            <ListItemText primary="QR Code" />
-          </ListItem>
-          <ListItem button component={NavLink} to="/admin/whiteboard">
-            <ListItemIcon>
-              <Gesture />
-            </ListItemIcon>
-            <ListItemText primary="Whiteboard" />
-          </ListItem>
-          <ListItem button onClick={handleLogout}>
-            <ListItemIcon>
-              <LogoutIcon />
-            </ListItemIcon>
-            <ListItemText primary="Logout" />
-          </ListItem>
-        </List>
-      </div>
-    </Drawer>
+    <>
+      {!isLargeDevice &&
+        <IconButton
+          onClick={handleToggle}
+          style={{ position: 'fixed', top: 10, left: 150, zIndex: 1300 }} // Keeps the button fixed
+        >
+          <MenuRounded />
+        </IconButton>
+      }
+      <Drawer variant="persistent" open={open}>
+        <div className="d-flex flex-column p-3" style={{ height: '100vh' }}>
+          <Typography variant="h6">Admin Panel</Typography>
+          <List>
+            <ListItem button component={NavLink} to="/admin/dashboard">
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/list">
+              <ListItemIcon>
+                <Newspaper />
+              </ListItemIcon>
+              <ListItemText primary="All Blogs" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/users">
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Users" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/settings">
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItem>
+            <Typography variant="h6" className='ml-4'>Apps</Typography>
+            <ListItem button component={NavLink} to="/admin/todo">
+              <ListItemIcon>
+                <Done />
+              </ListItemIcon>
+              <ListItemText primary="To Do" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/stopwatch">
+              <ListItemIcon>
+                <AccessAlarm />
+              </ListItemIcon>
+              <ListItemText primary="Clock" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/calculator">
+              <ListItemIcon>
+                <Calculate />
+              </ListItemIcon>
+              <ListItemText primary="Calculator" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/bmanager">
+              <ListItemIcon>
+                <Bookmark />
+              </ListItemIcon>
+              <ListItemText primary="Bookmark" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/qrcode">
+              <ListItemIcon>
+                <QrCode />
+              </ListItemIcon>
+              <ListItemText primary="QR Code" />
+            </ListItem>
+            <ListItem button component={NavLink} to="/admin/whiteboard">
+              <ListItemIcon>
+                <Gesture />
+              </ListItemIcon>
+              <ListItemText primary="Whiteboard" />
+            </ListItem>
+            <ListItem button onClick={handleLogout}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </>
   );
 };
